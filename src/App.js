0e847016cfcb90f0eadd2220df5bc2c4ea7ff2cf
hamid47.hrs <hamid47.hrs@gmail.com>
2022-02-25 20:56:49 +0330

    Repalce the Header with HomePage.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Header from "./components/header/Header";
+import HomePage from "./components/Home/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <HomePage />
     </div>
   );
 }
