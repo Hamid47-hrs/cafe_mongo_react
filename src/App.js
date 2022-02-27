d0686831085efc315b37100abdfe105f1f45f14e
hamid47.hrs <hamid47.hrs@gmail.com>
2022-02-27 23:34:35 +0330

    Add home, login and signup components to the app and create a route for each address.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./components/Home/HomePage";
+import LoginForm from "./components/Login/Login";
+import SignUpForm from "./components/signup/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path={"/"} component={HomePage} />
+          <Route exact path={"/login"} component={LoginForm} />
+          <Route exact path={"/signup"} component={SignUpForm} />
+        </Switch>
+      </Router>
     </div>
   );
 }
