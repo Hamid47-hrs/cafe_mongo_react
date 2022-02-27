df74215cbf53db2ef8093ec1e078abe19b0b69cf
hamid47.hrs <hamid47.hrs@gmail.com>
2022-02-27 23:32:59 +0330

    Create signup component with style.

---
+++ src/components/signup/SignUp.jsx
@@ -0,0 +1,151 @@
+import {
+  Button,
+  Typography,
+  Grid,
+  TextField,
+  Link,
+  FormControlLabel,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+} from "@material-ui/core";
+import logo from "../../images/icons/name_logo.png";
+import useStyle from "./SignUpStyles";
+
+const SignUpForm = () => {
+  const classes = useStyle();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.logoContainer}>
+        <img className={classes.logo} src={logo} alt="logo" />
+      </div>
+      <div className={classes.wellcomeContainer}>
+        <Typography className={classes.wellcomeText}>فرم ثبت نام</Typography>
+      </div>
+      <form className={classes.form}>
+        <Grid container className={classes.userGrid}>
+          <Grid Item>
+            <Grid item className={classes.userGridItem}>
+              <TextField
+                className={classes.userInput}
+                name="firstName"
+                label="نام"
+                type="text"
+                required
+              />
+            </Grid>
+            <Grid item className={classes.userGridItem}>
+              <TextField
+                className={classes.userInput}
+                name="lastName"
+                label="نام خانوادگی"
+                type="text"
+                required
+              />
+            </Grid>
+            <Grid item className={classes.userGridItem}>
+              <TextField
+                className={classes.userInput}
+                id="username"
+                name="usename"
+                label="نام کاربری"
+                type="text"
+                required
+              />
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid item className={classes.userGridItem}>
+              <TextField
+                className={classes.userInput}
+                name="useremail"
+                label="آدرس ایمیل"
+                type="email"
+                required
+              />
+            </Grid>
+            <Grid item className={classes.userGridItem}>
+              <TextField
+                className={classes.userInput}
+                id="userpassword"
+                name="userpassword"
+                label="رمز عبور"
+                required
+                type="password"
+              />
+            </Grid>
+            <Grid item className={classes.userGridItem}>
+              <TextField
+                className={classes.userInput}
+                name="userpasswordConfirm"
+                label="تکرار رمز عبور"
+                required
+                type="password"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <FormControlLabel
+          control={
+            <Checkbox
+              // checked={state.checkedB}
+              // onChange={handleChange}
+              name="checkedB"
+              color="primary"
+            />
+          }
+          className={classes.vipCheck}
+          label="آیا مایل به دریافت عضویت ویژه می باشید؟"
+        />
+        <FormControl className={classes.formControl}>
+          <InputLabel shrink className={classes.vipTitle}>
+            نوع اشتراک
+          </InputLabel>
+          <Select
+            // value={age}
+            // onChange={handleChange}
+            displayEmpty
+            className={classes.selectEmpty}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem className={classes.vipItems} value={1}>
+              یک ماهه : 30هزار تومان
+            </MenuItem>
+            <MenuItem className={classes.vipItems} value={3}>
+              سه ماهه : 90 هزار تومان
+            </MenuItem>
+            <MenuItem className={classes.vipItems} value={6}>
+              شش ماهه : 180 هزار تومان
+            </MenuItem>
+            <MenuItem className={classes.vipItems} value={12}>
+              یک ساله : 300 هزار تومان
+              <span className={classes.vipOffer} >(30 درصد تخفیف ویژه)</span>
+            </MenuItem>
+          </Select>
+          <FormHelperText className={classes.chooseOffer} >
+            نوع اشتراک مورد نظر خود را انتخاب کنید.
+          </FormHelperText>
+        </FormControl>
+        <Button type="submit" className={classes.submitButton}>
+          ثبت نام
+        </Button>
+        <Typography className={classes.didYouSignUp}>
+          <span className={classes.asterisk}>*</span>
+          قبلا ثبت نام کرده اید؟
+          <Link href="/login" className={classes.loginLink}>
+            اینجا
+          </Link>
+          کلیک کنید.
+        </Typography>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
---
+++ src/components/signup/SignUpStyles.js
@@ -0,0 +1,93 @@
+import { makeStyles } from "@material-ui/core";
+import backgroundPicture from "../../images/coffee_background_space_text_85121087.jpg";
+
+const useStyle = makeStyles((theme) => ({
+  container: {
+    background: `url(${backgroundPicture})`,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: theme.spacing(2),
+  },
+  logo: {
+    width: "12rem",
+    padding: theme.spacing(1),
+    backgroundColor: '#ffffff33',
+    borderRadius: '4px 15px'
+  },
+  wellcomeText: {
+    fontFamily: "vazir-bold",
+    fontSize: "1.5rem",
+    color: "#ffffff",
+    marginBottom: theme.spacing(1.5),
+  },
+  form: {
+    backgroundColor: "#fffffff1",
+    padding: "1rem 2rem",
+    borderRadius: 10,
+    width: "35%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  userGrid: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  userGridItem: {
+    width: "100%",
+  },
+  userInput: {
+    width: "100%",
+    direction: "rtl",
+    textAlign: "right",
+    marginBottom: theme.spacing(3),
+    "& .MuiFormLabel-root": {
+      fontFamily: "vazir",
+      textAlign: "right",
+    },
+  },
+  vipCheck: {
+    "& .MuiFormControlLabel-label": {
+      fontFamily: "vazir",
+    },
+  },
+  formControl: {
+    marginBottom: theme.spacing(2),
+  },
+  vipTitle: {
+    fontFamily: "vazir",
+  },
+  vipItems: {
+    fontFamily: "vazir",
+  },
+  vipOffer: {
+    color: "#c94646",
+    paddingRight: theme.spacing(1),
+  },
+  chooseOffer: {
+    fontFamily: "vazir",
+  },
+  submitButton: {
+    border: "solid 2px #A3DA8D",
+    padding: "0.4rem 7rem",
+    marginBottom: theme.spacing(4),
+    "&:hover": {
+      border: "solid 2px #36bee7",
+    },
+  },
+  didYouSignUp: {
+    fontFamily: "vazir",
+    fontSize: "1.1rem",
+  },
+  asterisk: {
+    color: "red",
+    marginLeft: theme.spacing(2),
+  },
+  loginLink: {
+    margin: "0 0.3rem",
+  },
+}));
+
+export default useStyle;
