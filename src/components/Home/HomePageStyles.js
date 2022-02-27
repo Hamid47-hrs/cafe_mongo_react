330181740940d5900085b6f3cdef8dc821d1e550
hamid47.hrs <hamid47.hrs@gmail.com>
2022-02-27 23:30:22 +0330

    Change the style of Home component to Flex.

--- src/components/Home/HomePageStyles.js
+++ src/components/Home/HomePageStyles.js
@@ -2,7 +2,8 @@ import { makeStyles } from "@material-ui/core";
 
 const useStyle = makeStyles((theme) => ({
   root: {
-    width: "100vw",
+    display: "flex",
+    flexDirection: "column",
   },
 }));
 
