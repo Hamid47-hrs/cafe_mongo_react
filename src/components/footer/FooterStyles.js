3ffad22c310adc6ebf48778a2563326e0059a04f
hamid47.hrs <hamid47.hrs@gmail.com>
2022-02-27 23:29:31 +0330

    Create a top margin for the footer component

--- src/components/footer/FooterStyles.js
+++ src/components/footer/FooterStyles.js
@@ -4,6 +4,7 @@ const useStyle = makeStyles((theme) => ({
   container: {
     display: "flex",
     padding: theme.spacing(1),
+    marginTop: theme.spacing(2),
     backgroundColor: '#a65214aa'
   },
 }));
