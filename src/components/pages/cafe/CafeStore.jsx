40068027dbae38d28f84f6ecd61505f1ae2a792b
hamid47.hrs <hamid47.hrs@gmail.com>
2022-03-06 20:27:23 +0330

    Create CafeStore component with full style.

---
+++ src/components/pages/cafe/CafeStore.jsx
@@ -0,0 +1,45 @@
+import ProductCard from "../../Home/main/product_navigator/ProductCard";
+import useStyle from "./CafeStoreStyles";
+import { useEffect, useState } from "react";
+
+const CafeStore = () => {
+  const classes = useStyle();
+  const [products, setProducts] = useState([]);
+
+  const getData = () => {
+    fetch("./product.json", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div className={classes.container}>
+        {products.map((item, index) => (
+          <ProductCard
+            key={index}
+            name={item.name}
+            type={item.type}
+            subset={item.subset}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+            NOP={item.NumberOfPurchases}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default CafeStore;
---
+++ src/components/pages/cafe/CafeStoreStyles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyle = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    backgroundColor: "#FFF1BD",
+  },
+}));
+
+export default useStyle;
