2460d1c0a274a953e92f4fbf174e5e82047e3654
hamid47.hrs <hamid47.hrs@gmail.com>
2022-03-06 20:29:31 +0330

    Add the Main component to the App and put the route of the pages in it and add the route of the CafeStore page.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./components/main/Main";
 import HomePage from "./components/Home/HomePage";
-import LoginForm from "./components/pages/Login/Login";
+import LoginForm from "./components/pages/login/Login";
 import SignUpForm from "./components/pages/signup/SignUp";
 import ProductPage from "./components/pages/productPage/ProductPage";
+import CafeStore from "./components/pages/cafe/CafeStore";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path={"/"} component={HomePage} />
           <Route exact path={"/login"} component={LoginForm} />
           <Route exact path={"/signup"} component={SignUpForm} />
-          <Route path={"/products/:productId"} component={ProductPage} />
+          <Main>
+            <Route exact path={"/"} component={HomePage} />
+            <Route path={"/products/:productId"} component={ProductPage} />
+            <Route path={"/cafe"} component={CafeStore} />
+          </Main>
         </Switch>
       </Router>
     </div>
