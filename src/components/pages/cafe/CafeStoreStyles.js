5981b6fe65bdf9a2b4597557d9a30769b4ab3fa7
hamid47.hrs <hamid47.hrs@gmail.com>
2022-03-07 20:09:50 +0330

    Add background image to component.

--- src/components/pages/cafe/CafeStoreStyles.js
+++ src/components/pages/cafe/CafeStoreStyles.js
@@ -1,11 +1,14 @@
 import { makeStyles } from "@material-ui/core";
+import image from "../../../images/14558b98bd1d7de23d23772ee22fccda7c4ea8e5.jpg";
 
 const useStyle = makeStyles((theme) => ({
   container: {
     display: "flex",
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    flexWrap: "wrap",
+    justifyContent: "space-around",
     backgroundColor: "#FFF1BD",
+    backgroundImage: `url(${image})`,
+    backgroundSize: "contain",
   },
 }));
 
