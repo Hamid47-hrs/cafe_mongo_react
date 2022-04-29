f0a620d8a8a6e4f39b0b51fcf7da891e00430941
hamid47.hrs <hamid47.hrs@gmail.com>
2022-04-29 19:45:26 +0430

    Create add and remove button, remove additional lines, create add and removeOneProduct functions.

--- src/components/pages/Cart/singleProduct/SingleProduct.jsx
+++ src/components/pages/Cart/singleProduct/SingleProduct.jsx
@@ -1,32 +1,58 @@
 import { Button, Typography } from "@material-ui/core";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import useStyle from "./SingleProductStyles";
-import image from "../../../../images/product_images/cafe/yellow_cocktail.jpg";
+import { useEffect, useState } from "react";
 
-const SingleProduct = () => {
+const SingleProduct = ({ name, subcet, price, image }) => {
   const classes = useStyle();
+  const [counter, setCounter] = useState(+1);
+  const [totalPrice, setTotalPrice] = useState(+price);
+
+  const removeOneItem = () => {
+    counter > 1 && setCounter(counter - 1);
+  };
+
+  useEffect(() => {
+    setTotalPrice(price * counter);
+  }, [counter, price]);
 
   return (
     <div className={classes.container}>
-      <div className={classes.productImageContaienr}>
-        <img src={image} className={classes.productImage} alt="product" />
+      <div className={classes.topContainer}>
+        <div className={classes.productImageContaienr}>
+          <img src={image} className={classes.productImage} alt="product" />
+        </div>
+        <div className={classes.productInfoContianer}>
+          <Typography className={classes.productTitle}>
+            {subcet + " " + name}
+          </Typography>
+          <Typography className={classes.productPrice}>
+            <span className={classes.titleText}>قیمت : </span>
+            {price} تومان
+          </Typography>
+        </div>
+        <Button className={classes.deleteButton}>
+          <DeleteForeverIcon className={classes.deleteButtonIcon} />
+        </Button>
+        <span className={classes.deleteButtonSpan}>
+          <DeleteForeverIcon className={classes.deleteButtonIcon} />
+        </span>
       </div>
-      <div className={classes.productInfoContianer}>
-        <Typography className={classes.productTitle}>
-          <span className={classes.titleText}>عنوان محصول : </span>
-          کوکتل زرد
+      <div className={classes.BottomContainer}>
+        <Button className={classes.removeOneButton} onClick={removeOneItem}>
+          -
+        </Button>
+        <Typography className={classes.numberOfProducts}>
+          <span className={classes.numberOfProducts}>تعداد :</span>
+          <span className={classes.numberOfProducts}>{counter}</span>
         </Typography>
-        <Typography className={classes.productPrice}>
-          <span className={classes.titleText}>قیمت محصول : </span>
-          3000 تومان
-        </Typography>
+        <Button
+          className={classes.addOneButton}
+          onClick={() => setCounter(counter + 1)}
+        >
+          +
+        </Button>
       </div>
-      <Button className={classes.deleteButton}>
-        <DeleteForeverIcon className={classes.deleteButtonIcon} />
-      </Button>
-      <span className={classes.deleteButtonSpan}>
-        <DeleteForeverIcon className={classes.deleteButtonIcon} />
-      </span>
     </div>
   );
 };
--- src/components/pages/Cart/singleProduct/SingleProductStyles.js
+++ src/components/pages/Cart/singleProduct/SingleProductStyles.js
@@ -2,9 +2,6 @@ import { makeStyles } from "@material-ui/core";
 
 const useStyle = makeStyles((theme) => ({
   container: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
     border: "solid 2px #872a0199",
     borderRadius: 4,
     padding: theme.spacing(1.5),
@@ -12,6 +9,11 @@ const useStyle = makeStyles((theme) => ({
     [theme.breakpoints.down("375")]: {
       padding: theme.spacing(0.5),
     },
+  },
+  topContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   productImageContaienr: {
     width: 50,
@@ -57,6 +59,30 @@ const useStyle = makeStyles((theme) => ({
     [theme.breakpoints.up("375")]: {
       display: "none",
     },
+  },
+  BottomContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "left",
+    marginTop: theme.spacing(2),
+  },
+  numberOfProducts: {
+    fontFamily: "vazir-medium",
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.down("425")]: {
+      fontSize: "0.7rem",
+    },
+  },
+  removeOneButton: {
+    backgroundColor: "#c94646",
+    color: "#000000aa",
+    border: "solid 2px #c71e1e",
+  },
+  addOneButton: {
+    backgroundColor: "#A3DA8D",
+    color: "#000000aa",
+    border: "solid 2px #38860c",
   },
 }));
 
