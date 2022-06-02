6db51cfab35a2aff11f2422e8ecb2f8bfff75bf5
hamid47.hrs <hamid47.hrs@gmail.com>
2022-06-02 22:35:11 +0430

    Create 'addToCart' function for ProductDetails component.

--- src/components/pages/productPage/productDetails/ProductDetails.jsx
+++ src/components/pages/productPage/productDetails/ProductDetails.jsx
@@ -1,27 +1,68 @@
 import { Button, Typography } from "@material-ui/core";
+import axios from "axios";
+import { toast } from "react-toastify";
 import useStyle from "./ProductDetailsStyles";
 
-const ProductDetails = ({prodTitle, prodImage, prodDesc, prodPrice}) => {
+const ProductDetails = ({
+  productId,
+  productName,
+  productSubset,
+  productTitle,
+  productImage,
+  productDesc,
+  productPrice,
+}) => {
   const classes = useStyle();
+
+  const addToCart = () => {
+    const newCart = {
+      userId: localStorage.getItem("user-id"),
+      productId: productId,
+      productName: productName,
+      productSubset: productSubset,
+      productPrice: productPrice,
+      productImage: productImage,
+    };
+
+    const config = {
+      headers: {
+        "x-auth-token": localStorage.getItem("x-auth-token"),
+      },
+    };
+
+    axios
+      .post("http://127.0.0.1:8080/cart/add-to-cart", newCart, config)
+      .than((res) => toast.success(res.data.message))
+      .catch((err) => toast.error(err.response.data.message));
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes.topContainer}>
         <div className={classes.rightBar}>
           <Typography className={classes.productTitle} varient="h1">
-            {prodTitle}
+            {productTitle}
           </Typography>
-          <Typography className={classes.productPrice}>{prodPrice} ریال</Typography>
+          <Typography className={classes.productPrice}>
+            {productPrice} ریال
+          </Typography>
           <div className={classes.buttonContainer}>
-            <Button className={classes.addToCart}>افزودن به سبد خرید</Button>
+            <Button className={classes.addToCart} onClick={addToCart}>
+              افزودن به سبد خرید
+            </Button>
           </div>
         </div>
         <div className={classes.leftBar}>
-          <img className={classes.productImage} src={prodImage} alt="product" />
+          <img
+            className={classes.productImage}
+            src={productImage}
+            alt="product"
+          />
         </div>
       </div>
       <div className={classes.bottomContainer}>
         <Typography className={classes.productDescription}>
-          {prodDesc}
+          {productDesc}
         </Typography>
       </div>
     </div>
